cf5029206a967e11d2509adb000dc4a84936d261
Z-67 <121440746+Z-67@users.noreply.github.com>
2023-07-25 15:45:32 +0100

    button clicks appear in display

--- script.js
+++ script.js
@@ -35,4 +35,48 @@ function operate (num1,operator,num2) {
             return num1 / num2;
     }
 }
- console.log(operate(5,'+',433));
+console.log(operate(5,'+',433));
+
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.querySelector(".display");
+    const plus = document.getElementById("plus");
+    const minus = document.getElementById("minus");
+    const multiply = document.getElementById("multiply");
+    const divide = document.getElementById("divide");
+    const clear = document.getElementById("clear");
+    const equals = document.getElementById("equals");
+    const btn0 = document.getElementById("0");
+    const btn1 = document.getElementById("1");
+    const btn2 = document.getElementById("2");
+    const btn3 = document.getElementById("3");
+    const btn4 = document.getElementById("4");
+    const btn5 = document.getElementById("5");
+    const btn6 = document.getElementById("6");
+    const btn7 = document.getElementById("7");
+    const btn8 = document.getElementById("8");
+    const btn9 = document.getElementById("9");
+
+    // Add event listeners for button clicks
+    plus.addEventListener("click", () => onscreenDisplay(plus.textContent));
+    minus.addEventListener("click", () => onscreenDisplay(minus.textContent));
+    multiply.addEventListener("click", () => onscreenDisplay(multiply.textContent));
+    divide.addEventListener("click", () => onscreenDisplay(divide.textContent));
+
+    btn0.addEventListener("click", () => onscreenDisplay(btn0.textContent));
+    btn1.addEventListener("click", () => onscreenDisplay(btn1.textContent));
+    btn2.addEventListener("click", () => onscreenDisplay(btn2.textContent));
+    btn3.addEventListener("click", () => onscreenDisplay(btn3.textContent));
+    btn4.addEventListener("click", () => onscreenDisplay(btn4.textContent));
+    btn5.addEventListener("click", () => onscreenDisplay(btn5.textContent));
+    btn6.addEventListener("click", () => onscreenDisplay(btn6.textContent));
+    btn7.addEventListener("click", () => onscreenDisplay(btn7.textContent));
+    btn8.addEventListener("click", () => onscreenDisplay(btn8.textContent));
+    btn9.addEventListener("click", () => onscreenDisplay(btn9.textContent));
+
+    // Function to update the display content
+    let onscreenDisplay = function (value) {
+        display.textContent += value;
+    };
+});
+    
+
