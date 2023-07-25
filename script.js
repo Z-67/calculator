54fc731ea2b31a41a03c0f7b483051235eda56db
Z-67 <121440746+Z-67@users.noreply.github.com>
2023-07-25 01:10:31 +0100

    javascript maths functions done

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+    console.log(a+b);
+
+}
+add(1,2);
+
+function subtract (a,b) {
+    console.log(a-b);
+}
+subtract(2,2)
+
+function multiply (a,b) {
+    console.log(a*b);
+}
+multiply(5,5)
+    
+function divide (a,b) {
+    console.log(a/b);
+}
+divide(10,2)
